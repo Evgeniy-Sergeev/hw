02c6f1e08de7d28498724c0a221ffb4579d93e48
Evgeniy-Sergeev <manalota7@gmail.com>
2024-10-09 13:35:18 +0300

    wer16.2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,46 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    """Базовый абстрактный класс для всех продуктов"""
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self._price = price
         self.quantity = quantity
+
+    @property
+    @abstractmethod
+    def price(self):
+        """Возвращает цену продукта"""
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        """Возвращает строковое представление продукта"""
+        pass
+
+    @price.setter
+    @abstractmethod
+    def price(self, new_price):
+        """Устанавливает новую цену продукта"""
+        pass
+
+
+class InfoMixin:
+    """Миксин для печати информации о создании объектов"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(f"Объект класса {self.__class__.__name__} создан с параметрами: {self.__dict__}")
+
+
+class Product(BaseProduct, InfoMixin):
+    """Класс, представляющий общий продукт, наследуется от BaseProduct и InfoMixin"""
+
+    def __init__(self, name, description, price, quantity):
+        super().__init__(name, description, price, quantity)
 
     @property
     def price(self):
@@ -16,15 +53,6 @@ class Product:
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
-    @classmethod
-    def new_product(cls, product_data):
-        return cls(
-            product_data["name"],
-            product_data["description"],
-            product_data["price"],
-            product_data["quantity"]
-        )
-
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
@@ -35,6 +63,8 @@ class Product:
 
 
 class Smartphone(Product):
+    """Класс Смартфон, наследуется от Product"""
+
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
@@ -44,6 +74,8 @@ class Smartphone(Product):
 
 
 class LawnGrass(Product):
+    """Класс Газонная трава, наследуется от Product"""
+
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
         self.country = country
@@ -52,6 +84,8 @@ class LawnGrass(Product):
 
 
 class Category:
+    """Класс категории продуктов"""
+
     product_count = 0
 
     def __init__(self, name, description, products=None):
@@ -78,7 +112,7 @@ class Category:
         return f"{self.name}, количество продуктов: {total_quantity} шт."
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -106,19 +140,15 @@ if __name__ == "__main__":
     print(category1.name == "Смартфоны")
     print(category1.description)
     print(len(category1.products))
-    print(category1.products)
-    print(category1.product_count)
+    print(Category.product_count)
 
     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
     category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром,"
-                         " станет вашим другом и помощником",
+                         "Современный телевизор, который позволяет наслаждаться просмотром, "
+                         "станет вашим другом и помощником",
                          [product4])
 
     print(category2.name)
     print(category2.description)
     print(len(category2.products))
     print(category2.products)
-
-    print(category1.products)
-    print(Category.product_count)
