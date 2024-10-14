d5c77a3528f582e050b1442eac4b00900de64e43
Evgeniy-Sergeev <manalota7@gmail.com>
2024-10-14 11:00:33 +0300

    wer17.1

--- src/main.py
+++ src/main.py
@@ -28,16 +28,10 @@ if __name__ == '__main__':
         print("Возникла ошибка:", e)
     else:
         print("Продукт создан успешно")
-
-    # Создание нескольких товаров
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    # Создание категории с товарами
     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
     print(f"Средняя цена в категории: {category1.middle_price()} руб.")
-
-    # Создание пустой категории
     category_empty = Category("Пустая категория", "Категория без продуктов", [])
     print(f"Средняя цена в пустой категории: {category_empty.middle_price()} руб.")
